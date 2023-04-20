325c198df7b50a338d8dab3d1d48ec99f5721b78
kb150802 <bansalkushagra150802@gmail.com>
2023-04-20 16:55:36 +0530

    E-Commerce-Dashboard

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div >
+            <img src="https://i.ibb.co/g9yrp8h/ecart.jpg"
+             className='homeimg' alt="Responsive image"></img>
+            <h1 className='slogan  font_arvo'>Progress With Every Purchase</h1>
+        </div>
+    )
+}
+
+export default Home;
